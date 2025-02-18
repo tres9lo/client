819e5b794409e55f9468d307853a4f6e8f0349a8
tres9lo <bikotreslo.99@gmail.com>
2025-02-18 19:44:30 +0200

    the art of css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BsFacebook, BsTwitterX, BsInstagram } from 'react-icons/bs';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div class="container">
+    <div class="left-bar"><h2>Sign In</h2>
+    <div class="tricircles">
+      <div class="circle"><BsFacebook /></div>
+      <div class="circle"><BsInstagram /></div>
+      <div class="circle"><BsTwitterX /></div>
+    </div>
+    <div class="suggestion">Or use your account</div>
+    <div class="form-container">
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <p>Forgot your password?</p>
+        <button>Sign In</button>
+        </form>
+      </div>
+  
+    </div>
+    <div class="right-bar">
+    <h2> Hello, Friend!</h2>
+    <p>Enter your personal details and start a</p><p> journey with us</p>
+    <button>Sign Up</button>
+    
+    </div>
     </div>
   );
 }

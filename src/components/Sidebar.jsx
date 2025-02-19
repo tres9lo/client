d02a4e9b09e95880eb9900287e7897bf554b0ef8
tres9lo <bikotreslo.99@gmail.com>
2025-02-19 17:15:52 +0200

    statunavigation  styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {  BsPencil, BsPersonFillCheck, BsThreeDotsVertical} from 'react-icons/bs';
+import {  BsPencil, BsPersonFillCheck, BsPlusCircle, BsThreeDotsVertical} from 'react-icons/bs';
 
 const Sidebar = () => {
   return (
@@ -17,7 +17,22 @@ const Sidebar = () => {
             <h3>STATUS</h3>
             <p>Available</p>
         </div>
+
+        <div className="projects">
+            <h3>PROJECTS</h3>
+            <p><BsPlusCircle/></p>
+            
+        </div>
+        <div className="project-list">
+                <p>Project 1</p>
+                <p>Project 2</p>
+                <p>Project 3</p>
+            </div>
         
+        <div className="other-content">
+            <h3>Other Content</h3>
+            <p>WorkFlows <div className="count">5</div></p>
+        </div>
         
     </div>
   )

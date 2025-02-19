60d59964accbec009d3aabc75a4bb6a247d48600
tres9lo <bikotreslo.99@gmail.com>
2025-02-19 18:45:32 +0200

    top nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { BsExclamationCircleFill } from 'react-icons/bs'
 
 const Navbar = () => {
   return (
-    <div className='nav-container'>Dev Community</div>
+    <div className='nav-container'><h3>Dev Community</h3> <h5><BsExclamationCircleFill/></div>
   )
 }
 
